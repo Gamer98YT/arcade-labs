5d7c1936d07213894792a5d53dc6b7d33d49fa15
Gamer98YT <izanbarrenagamer98gmail.com>
2026-02-05 20:29:17 +0100

    Se ha restaurado a la version original

--- lab01-git/lab01-git.py
+++ lab01-git/lab01-git.py
@@ -2,13 +2,10 @@ import arcade
 
 WIDTH = 800
 HEIGHT = 600
-print("Yo tambien soy un conflicto")
-
 arcade.open_window(WIDTH, HEIGHT, "Example")
 
 arcade.start_render()
 arcade.draw_text("Hello, world", 350, 300, arcade.color.WHITE)
 arcade.finish_render()
-print("Se fue el conflicto")
 
 arcade.run()
