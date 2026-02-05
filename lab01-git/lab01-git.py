24d6fc387641fc9dbe0e3757dd14336dd706c384
Gamer98YT <izanbarrenagamer98gmail.com>
2026-02-05 20:05:43 +0100

    Ha surgido un conflicto

--- lab01-git/lab01-git.py
+++ lab01-git/lab01-git.py
@@ -8,5 +8,6 @@ arcade.open_window(WIDTH, HEIGHT, "Example")
 arcade.start_render()
 arcade.draw_text("Hello, world", 350, 300, arcade.color.WHITE)
 arcade.finish_render()
+print("Buenas, soy un conflicto")
 
 arcade.run()
