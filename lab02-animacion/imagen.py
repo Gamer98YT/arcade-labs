caa43be6c48661a0ad2bb7ee8d80e9dbe83c11bc
Gamer98YT <izanbarrenagamer98gmail.com>
2026-02-12 19:44:22 +0100

    Implementacion de la funcion "dibujar_tractor"

--- lab02-animacion/imagen.py
+++ lab02-animacion/imagen.py
@@ -1,6 +1,38 @@
 
 import arcade
 
+def dibujar_tractor(x, y):
+    """
+    Dibuja un tractor en las coordenadas (x, y).
+    El punto (x, y) corresponde al centro de la rueda trasera grande.
+    """
+    
+    # --- Draw the engine ---
+    # Original: 600 (x+110), 120 (y+10)
+    arcade.draw_rect_filled(arcade.XYWH(x + 110, y + 10, 140, 70), arcade.color.GRAY)
+    # Original: 590 (x+100), 105 (y-5)
+    arcade.draw_rect_filled(arcade.XYWH(x + 100, y - 5, 90, 40), arcade.color.BLACK)
+
+    # --- Draw the smoke stack ---
+    # Original: 580 (x+90), 175 (y+65)
+    arcade.draw_rect_filled(arcade.XYWH(x + 90, y + 65, 10, 40), arcade.color.BLACK)
+
+    # --- Back wheel ---
+    # Esta es nuestra ancla (x, y)
+    arcade.draw_circle_filled(x, y, 50, arcade.color.BLACK)
+    arcade.draw_circle_filled(x, y, 45, arcade.color.BLACK_OLIVE)
+    arcade.draw_circle_filled(x, y, 35, arcade.color.OLD_LACE)
+    arcade.draw_circle_filled(x, y, 10, arcade.color.RED)
+
+    # --- Front wheel ---
+    # Original: 650 (x+160), 90 (y-20)
+    fx = x + 160
+    fy = y - 20
+    arcade.draw_circle_filled(fx, fy, 30, arcade.color.BLACK)
+    arcade.draw_circle_filled(fx, fy, 25, arcade.color.BLACK_OLIVE)
+    arcade.draw_circle_filled(fx, fy, 18, arcade.color.OLD_LACE)
+    arcade.draw_circle_filled(fx, fy, 5, arcade.color.RED)
+
 arcade.open_window(800, 600, "Dibujo fragger")
 
 # Color de fondo
@@ -28,6 +60,8 @@ arcade.draw_lrbt_rectangle_filled(600, 700, 100, 110, arcade.color.WHITE)
 arcade.draw_lrbt_rectangle_filled(720, 820, 100, 110, arcade.color.WHITE)
 
 # --- Draw the tractor ---
+
+dibujar_tractor(100, 110)
 
 # Draw the engine
 arcade.draw_rect_filled(arcade.XYWH(600, 120, 140, 70), arcade.color.GRAY)
